e50734243b02fc9553b1ac1ee7600047eac12a37
Sharonne24 <sawuor395@gmail.com>
2023-03-24 16:52:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+document.querySelector('main');
+
+const newHeader = document.querySelector('h1#victory');
+
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+
+ 
+
+
+
+
+
+
+
+
